55bab0bade51a4141aeaee9176af61ded27366f4
Siang Jun Teo <siangjun1707@gmail.com>
2019-11-11 13:42:05 +0000

    Implemented Location and LocationTest

--- src/uk/ac/ed/bikerental/Location.java
+++ src/uk/ac/ed/bikerental/Location.java
@@ -1,25 +1,59 @@
 package uk.ac.ed.bikerental;
 
+/**
+ * Location is the class which represents a spatial location described by:
+ * <ul>
+ *     <li>A postcode</li>
+ *     <li>An address</li>
+ * </ul>
+ *
+ * @author Michal Glinski
+ * @author Siang Jun Teo
+ */
 public class Location {
     private String postcode;
     private String address;
-    
+
+    /**
+     * Initialises an instance of class Location.
+     * @param postcode  The postcode of the object. Expects a string of
+     *                  length >= 6.
+     * @param address   The address of the object.
+     */
     public Location(String postcode, String address) {
         assert postcode.length() >= 6;
         this.postcode = postcode;
         this.address = address;
     }
-    
+
+
+    /**
+     * Tests whether the location is near a second {@link Location} passed as a parameter.
+     * @param other a second {@link Location}
+     * @return <code>true</code> if the location shares the same first two characters,
+     *         <code>false</code> otherwise.
+     */
     public boolean isNearTo(Location other) {
-        // TODO: Implement Location.isNearTo
-        assert false;
-        return true;
+        String otherPostcode = other.getPostcode();
+        if (this.postcode.charAt(0) == otherPostcode.charAt(0)) {
+            if (this.postcode.charAt(1) == otherPostcode.charAt(1)) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    /**
+     * @return the postcode of the location
+     */
     public String getPostcode() {
         return postcode;
     }
 
+
+    /**
+     * @return the address of the location
+     */
     public String getAddress() {
         return address;
     }
--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -4,10 +4,37 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 class TestLocation {
+    Location location1;
+    Location location2;
+    Location location3;
     @BeforeEach
     void setUp() throws Exception {
-        // TODO: setup some resources before each test
+        // Locations that are next to each other
+        location1 = new Location("EH8 9LE", "11 Crichton Street");
+        location2 = new Location("EH8 9LE", "10 Crichton Street");
+
+        // A location that isn't next to the other 2 locations
+        location3 = new Location("SW1A 2AA", "10 Downing Street");
     }
     
     // TODO: put some tests here
+    @Test
+    void testNear() {
+        assertEquals(true, location1.isNearTo(location2));
+    }
+
+    @Test
+    void testFar() {
+        assertEquals(false, location1.isNearTo(location3));
+    }
+
+    @Test
+    void testGetPostcode() {
+        assertEquals("EH8 9LE", location1.getPostcode());
+    }
+
+    @Test
+    void testGetAddress() {
+        assertEquals("11 Crichton Street", location1.getAddress());
+    }
 }
