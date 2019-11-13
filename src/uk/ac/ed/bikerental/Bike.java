966b073308dbb845b3242b913a1a44667b47a333
NolkMan <4n931mail@gmail.com>
2019-11-13 10:21:31 -0500

    Added javadoc to the Bike functions

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -40,10 +40,24 @@ public class Bike {
     public BikeType getType() {
         return this.type;
     }
+    
+    /**
+     * Checks whether bike is available for a give DateRange in a query
+     * @param query
+     * @return <code>boolean</code> whether is taken
+     */
     public boolean isTaken(Query query) {
     	return bookings.stream().anyMatch((booking) -> 
     		booking.getQuote().getQuery().getDateRange().overlaps(query.getDateRange()) );
     }
+    
+    /**
+     * Checks whether the bike is still available for the quote and if it is it creates a new Booking and makes 
+     * the bike unavailable for the time of the booking
+     * @param customer Customer is needed to create a booking
+     * @param quote Quote that the customer has chosen to book
+     * @return null if the bike is no longer available, a new object of Booking that represents booking that was made
+     */
     public Booking lock(Customer customer, Quote quote) {
     	// Make only one thread at a time to be possible to write to the set 
     	// Defensive as we don't know how the system may be designed when being implemented
@@ -65,6 +79,11 @@ public class Bike {
     	}
     	return booking;
     }
+
+    /**
+     * Removed the booking from the set so not litter code
+     * @param booking to be removed
+     */
     public void unlock(Booking booking) {
     	this.mutex.acquireUninterruptibly();
     	try {
