b557426d8d20e43a6ea6ccf5871cab334409d625
NolkMan <4n931mail@gmail.com>
2019-11-13 10:13:31 -0500

    Implemented lock and unlock functions in the Bike class

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -1,6 +1,8 @@
 package uk.ac.ed.bikerental;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.concurrent.Semaphore;
 
 /**
  * Class of each bike in the system.
@@ -8,9 +10,10 @@ import java.time.LocalDate;
  * @author Siang Jun Teo
  */
 public class Bike {
+    private Semaphore mutex;
     private BikeType type;
-    private boolean taken;
     private LocalDate dateAcquired;
+    private HashSet<Booking> bookings;
 
     /**
      * Creates an instance of {@link Bike} class. Sets the date acquired to now.
@@ -18,8 +21,9 @@ public class Bike {
      */
     public Bike(BikeType type) {
         this.type = type;
-        this.taken = false;
         this.dateAcquired = LocalDate.now();
+        this.bookings = new HashSet<Booking>();
+        this.mutex = new Semaphore(1);
     }
 
     /**
@@ -29,13 +33,44 @@ public class Bike {
      */
     public Bike(BikeType type, LocalDate dateAcquired) {
         this.type = type;
-        this.taken = false;
         this.dateAcquired = dateAcquired;
+        this.bookings = new HashSet<Booking>();
+        this.mutex = new Semaphore(1);
     }
     public BikeType getType() {
         return this.type;
     }
     public boolean isTaken(Query query) {
-        return this.taken;
+    	return bookings.stream().anyMatch((booking) -> 
+    		booking.getQuote().getQuery().getDateRange().overlaps(query.getDateRange()) );
+    }
+    public Booking lock(Customer customer, Quote quote) {
+    	// Make only one thread at a time to be possible to write to the set 
+    	// Defensive as we don't know how the system may be designed when being implemented
+    	Booking booking = null;
+    	this.mutex.acquireUninterruptibly();
+
+    	try {
+			if (this.isTaken(quote.getQuery())) {
+				// The booking time was taken in between somebody got a quote and booked it
+				// The booking will return null by default
+				booking = null;
+			} else {
+				// The booking is not taken
+				booking = new Booking(customer, quote);
+				bookings.add(booking);
+			}
+    	} finally {
+    		this.mutex.release();
+    	}
+    	return booking;
+    }
+    public void unlock(Booking booking) {
+    	this.mutex.acquireUninterruptibly();
+    	try {
+    		bookings.remove(booking);
+    	} finally {
+    		this.mutex.release();
+    	}
     }
 }
