04db518c7a208491797989a8449fe86b244f4fb8
Siang Jun Teo <siangjun1707@gmail.com>
2019-11-24 21:20:58 +0000

    made some JavaDoc clarification

--- tests/uk/ac/ed/bikerental/SystemTests.java
+++ tests/uk/ac/ed/bikerental/SystemTests.java
@@ -18,27 +18,30 @@ public class SystemTests {
     // You can add attributes here
     Server testServer;
     Query testQuery;
-    ArrayList<Provider> providers;
+    ArrayList<Provider> testProviders;
+    ArrayList<Quote> testQuotes;
     Customer c1;
+    Bike b1;
     @BeforeEach
     void setUp() throws Exception {
         // Setup mock delivery service before each tests
         DeliveryServiceFactory.setupMockDeliveryService();
         PaymentServiceFactory.setupMockPaymentService();
-        providers = new ArrayList<>();
+        testProviders = new ArrayList<>();
         Provider p1 = new Provider(new Location("EH3 9QG", "123 Fountainbridge"), 10.0);
         Provider p2 = new Provider(new Location("EH8 9LE", "1 Potterrow"), 20.0);
         p1.addPartner(p2);
         p2.addPartner(p1);
-        providers.add(p1);
-        providers.add(p2);
+        testProviders.add(p1);
+        testProviders.add(p2);
         c1 = new Customer(new Location("EH9 1SE", "16 East Mayfield"));
-        testServer = new Server(new MockServerData(providers));
+        b1 = new Bike()
+        testServer = new Server(new MockServerData(testProviders));
 
         // Don't care how query fetching to the server works so we make a test one
         testQuery = new Query(new Location("EH8 9LE", ""), new DateRange(
                 LocalDate.of(2019,11,1), LocalDate.of(2019,11,4)),
-                new BikeType((5.0)));
+                new BikeType((5.0), BikeType.SubType.Mountain));
     }
     
     // TODO: Write system tests covering the three main use cases
@@ -46,8 +49,8 @@ public class SystemTests {
 
     @Test
     void testMatchQuery() {
-        ArrayList<Quote> testQuotes = new ArrayList<>();
-
+        testQuotes = new ArrayList<>();
+        Quote q1 = new Quote()
         testQuotes.add()
         assertEquals(testServer.getQuotes(testQuery);
     }
